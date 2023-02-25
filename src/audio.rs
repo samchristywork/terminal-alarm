4066b796ec7665f3b5beece642f3517388956fec
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-02-25 09:55:11 -0600

    Feature: Commit remaining code changes
    This repo will soon be deprecated. I want to commit the remaining
    changes before I abandon the repo.

--- src/audio.rs
+++ src/audio.rs
@@ -2,6 +2,7 @@ use crate::wav;
 use fon::chan::{Ch16, Ch32};
 use fon::{Audio, Frame};
 use twang::ops::Gain;
+use twang::osc::Sine;
 use twang::osc::Triangle;
 use twang::Synth;
 
@@ -10,13 +11,14 @@ struct Processors {
 }
 
 pub fn generate_triangle_wave() {
-    let mut audio = Audio::<Ch16, 2>::with_silence(48_000, 48_000 * 5);
+    let mut audio = Audio::<Ch16, 2>::with_silence(48_000, 48_000 * 1); // 1 second
     let proc = Processors {
         triangle: Triangle::new(),
     };
 
     Synth::new(proc, |proc, frame: Frame<_, 2>| {
         let triangle = proc.triangle.step(440.0);
+        //let sin = Sine::new();
 
         frame.pan(Gain.step(triangle, Ch32::new(1.0 / 12.0)), 0.0)
     })
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use std::env::var;
+use std::fs::create_dir_all;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub mod alarm;
 pub mod audio;
 pub mod wav;
 
 fn main() {
+    let foo = var("HOME").unwrap() + "/.config/terminal-alarm/";
+    create_dir_all(foo.clone()).unwrap();
     audio::generate_triangle_wave();
+
+    alarm::create(
+        "Alarm in 5 seconds",
+        foo.as_str(),
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            + 5,
+    );
+
+    //let items = alarm::list(foo.as_str());
+
+    //for item in items {
+    //    println!("{}", item);
+    //}
+
+    alarm::monitor(foo.as_str());
 }
